5979931fcd0fb029c02e115f5faddf3c32481d4b
nhphong <nhphong2407@gmail.com>
2019-04-09 18:25:26 +0700

    Wrap up SurveysViewModelTest

--- app/src/test/java/com/nhphong/nimblesurveys/viewmodels/SurveysViewModelImplTest.kt
+++ app/src/test/java/com/nhphong/nimblesurveys/viewmodels/SurveysViewModelImplTest.kt
@@ -2,21 +2,22 @@ package com.nhphong.nimblesurveys.viewmodels
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.jraska.livedata.test
-import com.nhaarman.mockito_kotlin.any
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.verify
+import com.nhaarman.mockito_kotlin.*
 import com.nhphong.nimblesurveys.data.Survey
 import com.nhphong.nimblesurveys.data.SurveysRepository
+import com.nhphong.nimblesurveys.utils.StringResProvider
 import io.reactivex.Completable
 import io.reactivex.Single
 import io.reactivex.schedulers.Schedulers
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.io.IOException
 
 class SurveysViewModelImplTest {
 
   private lateinit var surveysRepository: SurveysRepository
+  private lateinit var stringResProvider: StringResProvider
   private lateinit var viewModel: SurveysViewModel
 
   @get:Rule
@@ -30,8 +31,18 @@ class SurveysViewModelImplTest {
       on { saveSurveysToDB(any()) }.thenReturn(Completable.complete())
     }
 
+    stringResProvider = mock {
+      on { fetchingSurveys }.thenReturn("Fetching surveys...")
+      on { noSurveysMessage }.thenReturn("There are no surveys")
+    }
+
     val immediateScheduler = Schedulers.from { it.run() }
-    viewModel = SurveysViewModelImpl(surveysRepository, immediateScheduler, immediateScheduler)
+    viewModel = SurveysViewModelImpl(
+      surveysRepository,
+      stringResProvider,
+      immediateScheduler,
+      immediateScheduler
+    )
   }
 
   @Test
@@ -45,16 +56,176 @@ class SurveysViewModelImplTest {
   }
 
   @Test
-  fun loadSurveys_happyCase() {
-    val observer = viewModel.surveys.test()
+  fun loadSurveys_HappyCase() {
+    val surveyObserver = viewModel.surveys.test()
+    val messageObserver = viewModel.message.test()
+
     viewModel.loadSurveys()
     verify(surveysRepository).loadSurveysFromDB()
     verify(surveysRepository).fetchSurveysFromApi()
     verify(surveysRepository).saveSurveysToDB(surveysFromApi)
 
-    observer
+    surveyObserver
       .assertHistorySize(2)
       .assertValueHistory(surveysFromDb, surveysFromApi)
+
+    messageObserver
+      .assertHistorySize(2)
+      .assertValueHistory("Fetching surveys...", "")
+  }
+
+  @Test
+  fun loadSurveys_ThereIsNoCacheInTheDB() {
+    doReturn(Single.just(emptyList<Survey>()))
+      .whenever(surveysRepository).loadSurveysFromDB()
+
+    val surveysObserver = viewModel.surveys.test()
+    val messageObserver = viewModel.message.test()
+
+    viewModel.loadSurveys()
+    verify(surveysRepository).loadSurveysFromDB()
+    verify(surveysRepository).fetchSurveysFromApi()
+    verify(surveysRepository).saveSurveysToDB(surveysFromApi)
+
+    surveysObserver
+      .assertHistorySize(2)
+      .assertValueHistory(emptyList(), surveysFromApi)
+
+    messageObserver
+      .assertHistorySize(2)
+      .assertValueHistory("Fetching surveys...", "")
+  }
+
+  @Test
+  fun loadSurveys_CacheWasFound_ButApiFailed() {
+    val error = Exception("Server is down")
+    doReturn(Single.error<List<Survey>>(error))
+      .whenever(surveysRepository).fetchSurveysFromApi()
+
+    val surveysObserver = viewModel.surveys.test()
+    val messageObserver = viewModel.message.test()
+    val snackBarMessageObserver = viewModel.snackBarMessage.test()
+
+    viewModel.loadSurveys()
+    verify(surveysRepository).loadSurveysFromDB()
+    verify(surveysRepository).fetchSurveysFromApi()
+    verifyNoMoreInteractions(surveysRepository) // No database caching was performed
+
+    surveysObserver
+      .assertHistorySize(1)
+      .assertValueHistory(surveysFromDb)
+
+    messageObserver
+      .assertHistorySize(2)
+      .assertValueHistory("Fetching surveys...", "Exception (Server is down)")
+
+    snackBarMessageObserver
+      .assertValue {
+        it.peekContent() == "Exception (Server is down)"
+      }
+  }
+
+  @Test
+  fun loadSurveys_ErrorOccursWhenLoadingSurveysFromDB() {
+    val error = IOException("Unable to access database")
+    doReturn(Single.error<List<Survey>>(error))
+      .whenever(surveysRepository).loadSurveysFromDB()
+
+    val surveysObserver = viewModel.surveys.test()
+    val messageObserver = viewModel.message.test()
+    val snackBarMessageObserver = viewModel.snackBarMessage.test()
+
+    viewModel.loadSurveys()
+    verify(surveysRepository).loadSurveysFromDB()
+    verify(surveysRepository).fetchSurveysFromApi()
+    verify(surveysRepository).saveSurveysToDB(surveysFromApi)
+
+    surveysObserver
+      .assertHistorySize(1)
+      .assertValueHistory(surveysFromApi)
+
+    messageObserver
+      .assertHistorySize(2)
+      .assertValueHistory("Fetching surveys...", "")
+
+    snackBarMessageObserver
+      .assertValue {
+        it.peekContent() == "IOException (Unable to access database)"
+      }
+  }
+
+  @Test
+  fun loadSurveys_FetchSurveysFromApiReturnsNothing() {
+    doReturn(Single.just(emptyList<Survey>()))
+      .whenever(surveysRepository).fetchSurveysFromApi()
+
+    val surveysObserver = viewModel.surveys.test()
+    val messageObserver = viewModel.message.test()
+
+    viewModel.loadSurveys()
+    verify(surveysRepository).loadSurveysFromDB()
+    verify(surveysRepository).fetchSurveysFromApi()
+    verify(surveysRepository).saveSurveysToDB(emptyList())
+
+    surveysObserver
+      .assertHistorySize(2)
+      .assertValueHistory(surveysFromDb, emptyList())
+
+    messageObserver
+      .assertHistorySize(2)
+      .assertValueHistory("Fetching surveys...", "There are no surveys")
+  }
+
+  @Test
+  fun loadSurveys_ErrorOccursWhenCachingResultFromApi() {
+    val error = IOException("Unable to access database")
+    doReturn(Completable.error(error))
+      .whenever(surveysRepository).saveSurveysToDB(any())
+
+    val surveysObserver = viewModel.surveys.test()
+    val messageObserver = viewModel.message.test()
+    val internalErrorMessageObserver = viewModel.internalErrorMessage.test()
+
+    viewModel.loadSurveys()
+    verify(surveysRepository).loadSurveysFromDB()
+    verify(surveysRepository).fetchSurveysFromApi()
+    verify(surveysRepository).saveSurveysToDB(surveysFromApi)
+
+    surveysObserver
+      .assertHistorySize(2)
+      .assertValueHistory(surveysFromDb, surveysFromApi)
+
+    messageObserver
+      .assertHistorySize(2)
+      .assertValueHistory("Fetching surveys...", "")
+
+    internalErrorMessageObserver
+      .assertValue {
+        it.peekContent() == "IOException (Unable to access database)"
+      }
+  }
+
+  @Test
+  fun reloadSurveys_HappyCase() {
+    val snackBarMessageObserver = viewModel.snackBarMessage.test()
+    val messageObserver = viewModel.message.test()
+    val surveysObserver = viewModel.surveys.test()
+
+    viewModel.reloadSurveys()
+    verify(surveysRepository).fetchSurveysFromApi()
+    verify(surveysRepository).saveSurveysToDB(surveysFromApi)
+
+    snackBarMessageObserver
+      .assertValue {
+        it.peekContent() == "Fetching surveys..."
+      }
+
+    messageObserver
+      .assertHistorySize(2)
+      .assertValueHistory("Fetching surveys...", "")
+
+    surveysObserver
+      .assertValue(surveysFromApi)
   }
 
   private companion object TestData {
